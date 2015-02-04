ea05972a179cfc0437d566603067b069badc2178
Jon Poler <jdp5087@gmail.com>
2015-02-04 17:02:03 -0500

    First commit for Pandora's Dropbox!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pandoras-dropbox
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pandoras_dropbox/core_test.clj
@@ -0,0 +1,7 @@
+(ns pandoras-dropbox.core-test
+  (:require [clojure.test :refer :all]
+            [pandoras-dropbox.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
